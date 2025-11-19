dd5329649df04c12bac6b2c883d3e5545caaba67
TaizunJ <jafri.taizun.s@gmail.com>
2025-11-19 14:28:27 -0800

    Refactor Code for guidlines

---
+++ test/RUN_TESTS_GR.sh
@@ -0,0 +1,46 @@
+
+
+# cd /home/tsjafri/TAIZUN/OpenROAD/test
+# ################### Commands to run GR on AES design.
+# /usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+#   ../build/bin/openroad_sproute -exit fastroute_aes45.tcl   | tee /home/tsjafri/TAIZUN/TESTS/runlogs_aes_fast.log
+# /usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+#   ../build/bin/openroad_sproute -exit cugr_aes45.tcl        | tee /home/tsjafri/TAIZUN/TESTS/runlogs_aes_cugr.log
+# /usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+#   ../build/bin/openroad_sproute -exit sproute_aes45.tcl     | tee /home/tsjafri/TAIZUN/TESTS/runlogs_aes_sproute.log
+
+
+
+# ################### Commands to run GR on JPEG design.
+# /usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+#   ../build/bin/openroad_sproute -exit fastroute_jpegsky130hd.tcl   | tee /home/tsjafri/TAIZUN/TESTS/runlogs_jpeg_fast.log
+# /usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+#   ../build/bin/openroad_sproute -exit cugr_jpegsky130hd.tcl        | tee /home/tsjafri/TAIZUN/TESTS/runlogs_jpeg_cugr.log
+# /usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+#   ../build/bin/openroad_sproute -exit sproute_jpegsky130hd.tcl     | tee /home/tsjafri/TAIZUN/TESTS/runlogs_jpeg_sproute.log
+
+
+
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+
+cd /home/tsjafri/TAIZUN/OpenROAD/test
+/usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+  ../build/bin/openroad_spR_int -no_init -exit fastroute_aes45.tcl   | tee /home/tsjafri/TAIZUN/TESTS/AES/runlogs_aes_fast.log
+# /usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+#   ../build/bin/openroad_spR_int -no_init -exit cugr_aes45.tcl        | tee /home/tsjafri/TAIZUN/TESTS/AES/runlogs_aes_cugr.log
+/usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+  ../build/bin/openroad_spR_int -no_init -exit sproute_aes45.tcl     | tee /home/tsjafri/TAIZUN/TESTS/AES/runlogs_aes_sproute.log
+
+
+
+################### Commands to run GR on JPEG design.
+/usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+  ../build/bin/openroad_spR_int -no_init -exit fastroute_jpegsky130hd.tcl   | tee /home/tsjafri/TAIZUN/TESTS/JPEG/runlogs_jpeg_fast.log
+/usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+  ../build/bin/openroad_spR_int -no_init -exit cugr_jpegsky130hd.tcl        | tee /home/tsjafri/TAIZUN/TESTS/JPEG/runlogs_jpeg_cugr.log
+/usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+  ../build/bin/openroad_spR_int -no_init -exit sproute_jpegsky130hd.tcl     | tee /home/tsjafri/TAIZUN/TESTS/JPEG/runlogs_jpeg_sproute.log
--- test/cugr_jpegsky130hd.tcl
+++ test/cugr_jpegsky130hd.tcl
@@ -1,10 +1,13 @@
 set router_label "CUGR"
 set extra_router_args {-use_cugr}
 set post_gr_db "/home/tsjafri/TAIZUN/TESTS/JPEG/jpeg_cugr_after_gr.db"
+set post_dr_db "/home/tsjafri/TAIZUN/TESTS/JPEG/jpeg_cugr_after_dr.db"
+set dr_drc_rpt "/home/tsjafri/TAIZUN/TESTS/JPEG/jpeg_cugr_drc.rpt"
+set dr_stats_rpt "/home/tsjafri/TAIZUN/TESTS/JPEG/jpeg_cugr_dr_stats.rpt"
 
 rename ::global_route ::orig_global_route
 proc ::global_route {args} {
-  global extra_router_args router_label post_gr_db
+  global extra_router_args router_label post_gr_db post_dr_db dr_drc_rpt dr_stats_rpt
   set final_args [concat $args $extra_router_args]
   set t0 [clock milliseconds]
   uplevel 1 [list ::orig_global_route] $final_args
@@ -12,6 +15,16 @@ proc ::global_route {args} {
   puts "$router_label global_route took $dt ms"
   if {$post_gr_db ne ""} {
     write_db $post_gr_db
+  }
+  # run detailed routing for reporting purposes; GR timing already captured above
+  if {[info commands detailed_route] ne ""} {
+    detailed_route \
+      -output_drc $dr_drc_rpt \
+      -output_guide "" \
+      -output_maze $dr_stats_rpt
+  }
+  if {$post_dr_db ne ""} {
+    write_db $post_dr_db
   }
   exit
 }
