479b66dccaea3604f31ec7177e6d49d4dc7261cf
TaizunJ <jafri.taizun.s@gmail.com>
2025-11-25 00:03:05 -0800

    Added sproute_tool following guidlines

--- test/RUN_TESTS_GR.sh
+++ test/RUN_TESTS_GR.sh
@@ -30,17 +30,17 @@
 cd /home/tsjafri/TAIZUN/OpenROAD/test
 /usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
   ../build/bin/openroad_spR_int -no_init -exit fastroute_aes45.tcl   | tee /home/tsjafri/TAIZUN/TESTS/AES/runlogs_aes_fast.log
+# # /usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+# #   ../build/bin/openroad_spR_int -no_init -exit cugr_aes45.tcl        | tee /home/tsjafri/TAIZUN/TESTS/AES/runlogs_aes_cugr.log
 # /usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
-#   ../build/bin/openroad_spR_int -no_init -exit cugr_aes45.tcl        | tee /home/tsjafri/TAIZUN/TESTS/AES/runlogs_aes_cugr.log
-/usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
-  ../build/bin/openroad_spR_int -no_init -exit sproute_aes45.tcl     | tee /home/tsjafri/TAIZUN/TESTS/AES/runlogs_aes_sproute.log
+#   ../build/bin/openroad_spR_int -no_init -exit sproute_aes45.tcl     | tee /home/tsjafri/TAIZUN/TESTS/AES/runlogs_aes_sproute.log
 
 
 
-################### Commands to run GR on JPEG design.
-/usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
-  ../build/bin/openroad_spR_int -no_init -exit fastroute_jpegsky130hd.tcl   | tee /home/tsjafri/TAIZUN/TESTS/JPEG/runlogs_jpeg_fast.log
-/usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
-  ../build/bin/openroad_spR_int -no_init -exit cugr_jpegsky130hd.tcl        | tee /home/tsjafri/TAIZUN/TESTS/JPEG/runlogs_jpeg_cugr.log
-/usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
-  ../build/bin/openroad_spR_int -no_init -exit sproute_jpegsky130hd.tcl     | tee /home/tsjafri/TAIZUN/TESTS/JPEG/runlogs_jpeg_sproute.log
+# ################### Commands to run GR on JPEG design.
+# /usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+#   ../build/bin/openroad_spR_int -no_init -exit fastroute_jpegsky130hd.tcl   | tee /home/tsjafri/TAIZUN/TESTS/JPEG/runlogs_jpeg_fast.log
+# /usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+#   ../build/bin/openroad_spR_int -no_init -exit cugr_jpegsky130hd.tcl        | tee /home/tsjafri/TAIZUN/TESTS/JPEG/runlogs_jpeg_cugr.log
+# /usr/bin/time -f 'WALL=%E USER=%U SYS=%S' \
+#   ../build/bin/openroad_spR_int -no_init -exit sproute_jpegsky130hd.tcl     | tee /home/tsjafri/TAIZUN/TESTS/JPEG/runlogs_jpeg_sproute.log
