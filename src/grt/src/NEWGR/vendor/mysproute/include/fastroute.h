b30089952e1bc2cd8610a37cc1c13bd0ab8cb401
TaizunJ <jafri.taizun.s@gmail.com>
2026-02-21 05:05:36 +0000

    Iteration 3

--- src/grt/src/NEWGR/vendor/mysproute/include/fastroute.h
+++ src/grt/src/NEWGR/vendor/mysproute/include/fastroute.h
@@ -692,10 +692,10 @@ void runFastRoute(parser::grGenerator grGen, string benchFile, string OutFileNam
 	    
 		// call FLUTE to generate RSMT and break the nets into segments (2-pin nets)
 
-		VIA=2;
+		VIA=3;
 		//viacost = VIA;
 		// Enable via-aware FLUTE topology generation to bias toward fewer vias.
-		viacost = 1;
+		viacost = 2;
 		gen_brk_RSMT(FALSE, FALSE, FALSE, FALSE, noADJ);
 		printf("first L\n");
 		routeLAll(TRUE);
@@ -725,7 +725,7 @@ void runFastRoute(parser::grGenerator grGen, string benchFile, string OutFileNam
 		if (maxOverflow > 700) {
 			costheight = 8;
 			LOGIS_COF = 1.33;
-			VIA = 1;
+			VIA = 2;
 			THRESH_M = 0;
 			CSTEP1 = 30;
 			slope = BIG_INT;
@@ -822,7 +822,7 @@ void runFastRoute(parser::grGenerator grGen, string benchFile, string OutFileNam
 				slope = BIG_INT;
 				//slope = 20;
 				if (i == 5) {
-					VIA = 1;
+					VIA = 2;
 					LOGIS_COF = 1.33;
 					ripup_threshold = -1;
 				//	cost_type = 3;
