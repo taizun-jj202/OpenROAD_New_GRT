97fcad0acea92d178c80a7c6f1c37e93fd9b087e
TaizunJ <jafri.taizun.s@gmail.com>
2026-02-21 05:23:07 +0000

    Iteration 5

--- src/grt/src/NEWGR/vendor/mysproute/include/global.h
+++ src/grt/src/NEWGR/vendor/mysproute/include/global.h
@@ -12,22 +12,22 @@ int acc_count;
 int n_small_undone;
 float max_rudy;
 
-#define M2_ADJ_MIN 0.63
-#define M2_ADJ_MAX 0.98
+#define M2_ADJ_MIN 0.61
+#define M2_ADJ_MAX 0.97
 #define M2_ADJ_MID 3.5
 #define M2_ADJ_K 2.0
 
-#define M3_ADJ_MIN 0.63
-#define M3_ADJ_MAX 0.98
+#define M3_ADJ_MIN 0.61
+#define M3_ADJ_MAX 0.97
 #define M3_ADJ_MID 3.5
 #define M3_ADJ_K 2.0
 
-#define MID_ADJ_MIN 0.96
-#define MID_ADJ_MAX 0.96
+#define MID_ADJ_MIN 0.955
+#define MID_ADJ_MAX 0.955
 #define MID_ADJ_MID 5.0
 #define MID_ADJ_K 2.0
 
-#define HIGH_ADJ 0.88
+#define HIGH_ADJ 0.86
 
 #define OBS_NO_STOP 0 // 1 == go through OBS, 0 == hard stop
 
