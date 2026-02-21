a7e78e3ac5aa8d5781b1ba3113209572de8fd560
TaizunJ <jafri.taizun.s@gmail.com>
2026-02-21 18:28:00 +0000

    Iteration 38

--- src/grt/src/NEWGR/NewGR.cpp
+++ src/grt/src/NEWGR/NewGR.cpp
@@ -1,6 +1,8 @@
 #include "NEWGR/NewGR.h"
 
+#include <algorithm>
 #include <memory>
+#include <tuple>
 #include <vector>
 
 #include "NEWGR/src/NewgrEngine.h"
@@ -9,6 +11,68 @@
 #include "utl/Logger.h"
 
 namespace grt {
+
+namespace {
+
+bool isViaSegment(const GSegment& segment)
+{
+  return segment.init_x == segment.final_x && segment.init_y == segment.final_y
+         && segment.init_layer != segment.final_layer;
+}
+
+void sortRouteForMerge(GRoute& route)
+{
+  std::sort(route.begin(), route.end(), [](const GSegment& lhs, const GSegment& rhs) {
+    const bool lhs_is_via = isViaSegment(lhs);
+    const bool rhs_is_via = isViaSegment(rhs);
+    if (lhs_is_via != rhs_is_via) {
+      return !lhs_is_via;
+    }
+
+    if (lhs_is_via) {
+      const auto lhs_key = std::make_tuple(std::min(lhs.init_layer, lhs.final_layer),
+                                           std::max(lhs.init_layer, lhs.final_layer),
+                                           lhs.init_x,
+                                           lhs.init_y);
+      const auto rhs_key = std::make_tuple(std::min(rhs.init_layer, rhs.final_layer),
+                                           std::max(rhs.init_layer, rhs.final_layer),
+                                           rhs.init_x,
+                                           rhs.init_y);
+      return lhs_key < rhs_key;
+    }
+
+    const bool lhs_vertical = lhs.init_x == lhs.final_x;
+    const bool rhs_vertical = rhs.init_x == rhs.final_x;
+    if (lhs.init_layer != rhs.init_layer) {
+      return lhs.init_layer < rhs.init_layer;
+    }
+    if (lhs_vertical != rhs_vertical) {
+      return lhs_vertical < rhs_vertical;
+    }
+
+    if (lhs_vertical) {
+      const auto lhs_key
+          = std::make_tuple(lhs.init_x,
+                            std::min(lhs.init_y, lhs.final_y),
+                            std::max(lhs.init_y, lhs.final_y));
+      const auto rhs_key
+          = std::make_tuple(rhs.init_x,
+                            std::min(rhs.init_y, rhs.final_y),
+                            std::max(rhs.init_y, rhs.final_y));
+      return lhs_key < rhs_key;
+    }
+
+    const auto lhs_key = std::make_tuple(lhs.init_y,
+                                         std::min(lhs.init_x, lhs.final_x),
+                                         std::max(lhs.init_x, lhs.final_x));
+    const auto rhs_key = std::make_tuple(rhs.init_y,
+                                         std::min(rhs.init_x, rhs.final_x),
+                                         std::max(rhs.init_x, rhs.final_x));
+    return lhs_key < rhs_key;
+  });
+}
+
+}  // namespace
 
 NewGR::NewGR(GlobalRouter* grouter, CUGR* cugr, utl::Logger* logger)
     : grouter_(grouter), cugr_(cugr), logger_(logger)
@@ -43,6 +107,7 @@ NetRouteMap NewGR::run(std::vector<Net*>& nets,
   for (auto& net_route : routes) {
     std::vector<Pin>& pins = grouter_->db_net_map_[net_route.first]->getPins();
     GRoute& route = net_route.second;
+    sortRouteForMerge(route);
     grouter_->mergeSegments(pins, route);
   }
 
