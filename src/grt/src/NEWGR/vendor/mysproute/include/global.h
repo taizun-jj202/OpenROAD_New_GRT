1e22dd59ee23d491a766cf29950f30d72b4bd72b
TaizunJ <jafri.taizun.s@gmail.com>
2026-02-21 08:55:44 +0000

    Iteration 25

--- src/grt/src/NEWGR/vendor/mysproute/include/global.h
+++ src/grt/src/NEWGR/vendor/mysproute/include/global.h
@@ -12,26 +12,26 @@ int acc_count;
 int n_small_undone;
 float max_rudy;
 
-#define M2_ADJ_MIN 0.60
-#define M2_ADJ_MAX 0.96
+#define M2_ADJ_MIN 0.58
+#define M2_ADJ_MAX 0.95
 #define M2_ADJ_MID 3.5
 #define M2_ADJ_K 2.0
 
-#define M3_ADJ_MIN 0.60
-#define M3_ADJ_MAX 0.96
+#define M3_ADJ_MIN 0.58
+#define M3_ADJ_MAX 0.95
 #define M3_ADJ_MID 3.5
 #define M3_ADJ_K 2.0
 
-#define MID_ADJ_MIN 0.95
-#define MID_ADJ_MAX 0.95
+#define MID_ADJ_MIN 0.94
+#define MID_ADJ_MAX 0.94
 #define MID_ADJ_MID 5.0
 #define MID_ADJ_K 2.0
 
-#define HIGH_ADJ 0.85
+#define HIGH_ADJ 0.82
 
 #define OBS_NO_STOP 0 // 1 == go through OBS, 0 == hard stop
 
-#define DEFAULT_RUDY_WEIGHT 0.11
+#define DEFAULT_RUDY_WEIGHT 0.12
 #define NO_RUDY 0
 #define DEFAULT_PIN_DENSITY_WEIGHT 1.0
 
