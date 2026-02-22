a716102092fa0c4433b0c45c8da476cea3592d7c
TaizunJ <jafri.taizun.s@gmail.com>
2026-02-22 13:08:06 +0000

    Iteration 53

--- src/grt/src/NEWGR/src/CUGR.cpp
+++ src/grt/src/NEWGR/src/CUGR.cpp
@@ -98,7 +98,7 @@ void CUGR::patternRouteWithDetours(std::vector<int>& netIndices)
     int detourThreshold = constants_.detour_overflow_threshold;
     const int hp = net->getBoundingBox().hp();
     const int pins = net->getNumPins();
-    if (pins > 24 || hp > 160) {
+    if (pins > 16 || hp > 120) {
       detourThreshold = std::max(1, detourThreshold - 1);
     }
 
