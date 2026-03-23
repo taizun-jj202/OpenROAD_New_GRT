ea7f3185cce95150b8bdd1310112e86095415770
TaizunJ <jafri.taizun.s@gmail.com>
2026-03-23 14:29:27 +0000

    Iteration 27

--- src/grt/src/NEWGR/include/CUGR.h
+++ src/grt/src/NEWGR/include/CUGR.h
@@ -79,15 +79,15 @@ struct Constants
   bool enable_wirelength_recovery = true;
   bool recovery_use_maze = true;
   bool recovery_use_wirelength_maze = true;
-  double recovery_wl_maze_via_cost_scale = 0.15;
-  int recovery_wl_only_hpwl_threshold = 96;
-  int recovery_hpwl_threshold = 64;
-  double recovery_refine_ratio = 1.0;
+  double recovery_wl_maze_via_cost_scale = 0.08;
+  int recovery_wl_only_hpwl_threshold = 80;
+  int recovery_hpwl_threshold = 56;
+  double recovery_refine_ratio = 0.95;
   int recovery_max_passes = 5;
   double recovery_pass_decay = 0.86;
   int recovery_maze_sparse_x = 3;
   int recovery_maze_sparse_y = 3;
-  double recovery_deep_ratio = 0.65;
+  double recovery_deep_ratio = 0.6;
   int recovery_deep_hpwl_threshold = 140;
   int recovery_aniso_sparse_long = 7;
   int recovery_aniso_sparse_short = 3;
@@ -97,11 +97,11 @@ struct Constants
   int recovery_dense_sparse_x = 2;
   int recovery_dense_sparse_y = 2;
   int recovery_max_maze_configs = 30;
-  int recovery_max_via_increase = 4;
-  bool recovery_use_full_grid_maze = true;
-  int recovery_full_grid_top_n = 16;
-  int recovery_full_grid_hpwl_threshold = 220;
-  double recovery_full_grid_via_cost_scale = 0.08;
+  int recovery_max_via_increase = 6;
+  bool recovery_use_full_grid_maze = false;
+  int recovery_full_grid_top_n = 8;
+  int recovery_full_grid_hpwl_threshold = 260;
+  double recovery_full_grid_via_cost_scale = 0.04;
 
   double pin_patch_threshold = 1000000.0;
   int pin_patch_padding = 3;
