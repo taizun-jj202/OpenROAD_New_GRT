ed88b468dd4c48c0eed7e23201607d25a8b7e89a
TaizunJ <jafri.taizun.s@gmail.com>
2026-03-24 22:52:38 +0000

    Iteration 71

--- src/grt/src/NEWGR/include/CUGR.h
+++ src/grt/src/NEWGR/include/CUGR.h
@@ -88,6 +88,7 @@ class CUGR
   void finalPatternTighten();
   void globalCompaction();
   void strictWirelengthCompaction();
+  void ultraWirelengthPolish();
   void sortNetIndices(std::vector<int>& netIndices) const;
   void getGuides(const GRNet* net,
                  std::vector<std::pair<int, BoxT>>& guides);
