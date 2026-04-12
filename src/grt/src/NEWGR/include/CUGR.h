c6abef509bbd1832f9e416bfca65a01d3b32e7eb
TaizunJ <jafri.taizun.s@gmail.com>
2026-04-12 19:53:21 +0000

    Iteration 8

--- src/grt/src/NEWGR/include/CUGR.h
+++ src/grt/src/NEWGR/include/CUGR.h
@@ -31,21 +31,21 @@ struct Constants
 {
   // Wirelength-first tuning: reduce congestion over-penalization and let
   // the solver preserve shorter trees unless overflow is severe.
-  double weight_wire_length = 1.55;
-  double weight_via_number = 0.65;
-  double weight_short_area = 80.0;
+  double weight_wire_length = 1.60;
+  double weight_via_number = 0.62;
+  double weight_short_area = 75.0;
 
   int min_routing_layer = 1;
 
   double cost_logistic_slope = 1.0;
 
   // allowed stem length increase to trunk length ratio
-  double max_detour_ratio = 0.01;
+  double max_detour_ratio = 0.008;
   int target_detour_count = 1;
 
   double via_multiplier = 1.0;
 
-  double maze_logistic_slope = 0.15;
+  double maze_logistic_slope = 0.12;
 
   double pin_patch_threshold = 20.0;
   int pin_patch_padding = 1;
